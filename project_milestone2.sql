464a1da51ab3c5fe7b243436daa77fe898d17492
Zeina <zeinaayman666@gmail.com>
2022-12-05 19:51:02 +0200

    start

---
+++ project_milestone2.sql
@@ -0,0 +1,26 @@
+﻿create database ehna;
+
+create table S_User(
+username varchar(20) not null primary key,
+password varchar(20) not null
+);
+
+create table System_Admin(
+ID int not null Identity primary key,
+name varchar(20) not null,
+username varchar(20) not null,
+password varchar(20) not null,
+constraint SA_inheretance foreign key(username) references S_User(username)
+);
+
+create table Sports_Assocciation_Manager(
+ID int not null Identity primary key,
+name varchar(20) not null,
+username varchar(20) not null,
+password varchar(20) not null,
+constraint SAM_inheretance foreign key(username) references S_User(username)
+);
+
+
+
+
