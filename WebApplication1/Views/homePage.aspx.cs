a70daa06ad39f255f73d8b06d1fde608b6634667
Zeina <zeinaayman666@gmail.com>
2022-12-25 00:40:44 +0200

    home page kinda worked finally

--- WebApplication1/Views/homePage.aspx.cs
+++ WebApplication1/Views/homePage.aspx.cs
@@ -60,33 +60,74 @@ namespace WebApplication1.Views
 
         protected void LoginButton_Click(object sender, EventArgs e)
         {
+
             string username = UsernameTextBox.Text;
             string password = PasswordTextBox.Text;
-            string loginQuery = "select su.username, su.systemUserPassword\r\nfrom systemUser su\r\nwhere username = " + username + " and systemUserPassword = " + password + ";";
+
+            string loginQuery = "select su.username, su.systemUserPassword\r\nfrom systemUser su\r\nwhere username = '" + username + "' and systemUserPassword = '" + password + "';";
+            string systemAdminLogin = "select sa.systemAdminUsername\r\nfrom systemAdmin sa\r\nwhere sa.systemAdminUsername = '"+ username +"';";
+            string sportsAssocitionManagerLogin = "select sam.sportsAssociationManagerUsername\r\nfrom sportsAssociationManager sam\r\nwhere sam.sportsAssociationManagerUsername = '"+ username +"';";
+            string stadiumManagerLogin = "select sm.stadiumManagerUsername\r\nfrom stadiumManager sm\r\nwhere sm.stadiumManagerUsername = '"+ username +"';";
+            string clubRepresentativeLogin = "select cr.Username\r\nfrom clubRepresentative cr\r\nwhere cr.Username = '"+ username +"';";
+            string fanLogin = "select username\r\nfrom fan \r\nwhere username = '"+ username +"';";
             
+            string usernameReader = "";
 
-            try
-            {
-                SqlCommand loginCMD = new SqlCommand(loginQuery, db.con);
-                //SqlDataReader loginDataReader = loginCMD.ExecuteReader();
-                MessageBox.Show(loginCMD.ToString());
-                if (loginCMD.ToString() == "")
-                {
-                    System.Windows.Forms.MessageBox.Show("Username or Password does not exist");
-                    Console.WriteLine("null");
-                    return;
-                }
-                else
-                {
-                    MessageBox.Show("username and password exists");
-                    Console.WriteLine("done");
-                }
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("failed");
+            SqlCommand loginCMD = new SqlCommand(loginQuery, db.con);
+            SqlCommand systemAdminCMD = new SqlCommand(systemAdminLogin, db.con);
+            SqlCommand sportsAssociationManagerCMD = new SqlCommand(sportsAssocitionManagerLogin, db.con);
+            SqlCommand stadiumManagerCMD =  new SqlCommand(stadiumManagerLogin, db.con);
+            SqlCommand clubRepresentativeCMD = new SqlCommand(clubRepresentativeLogin, db.con);
+            SqlCommand fanCMD = new SqlCommand(fanLogin, db.con);
 
+            SqlDataReader loginReader = loginCMD.ExecuteReader();
+
+            while (loginReader.Read())
+            {
+                usernameReader = loginReader["username"].ToString();
             }
+
+            loginReader.Close();
+
+
+            //ig it's easier done with cases
+            //they all need while loops
+
+            if (usernameReader == "") MessageBox.Show("username or password doesn't exist");
+            else
+            {
+                SqlDataReader systemAdminReader = systemAdminCMD.ExecuteReader();
+                while (systemAdminReader.Read())
+                {
+                    if (usernameReader == systemAdminReader["systemAdminUsername"].ToString())
+                    {
+                        MessageBox.Show("system admin");
+                        
+                    }
+                }
+                systemAdminReader.Close();
+
+                SqlDataReader sportsAssociationManagerReader = sportsAssociationManagerCMD.ExecuteReader();
+                sportsAssociationManagerReader.Read();
+                if (usernameReader == sportsAssociationManagerReader["sportsAssociationManagerUsername"].ToString()) MessageBox.Show("sports association manager");
+                sportsAssociationManagerReader.Close();
+
+                SqlDataReader stadiumManagerReader = stadiumManagerCMD.ExecuteReader();
+                stadiumManagerReader.Read();
+                if (usernameReader == stadiumManagerReader["stadiumManagerUsername"].ToString()) MessageBox.Show("stadium manager");
+                stadiumManagerReader.Close();
+
+                SqlDataReader clubRepresentativeReader = clubRepresentativeCMD.ExecuteReader();
+                clubRepresentativeReader.Read();
+                if (usernameReader == clubRepresentativeReader["Username"].ToString()) MessageBox.Show("club representative");
+                clubRepresentativeReader.Close();
+
+                SqlDataReader fanReader = fanCMD.ExecuteReader();
+                fanReader.Read();
+                if (usernameReader == fanReader["username"].ToString()) MessageBox.Show("fan");
+                fanReader.Close();
+            }
+
         }
     }
 }
