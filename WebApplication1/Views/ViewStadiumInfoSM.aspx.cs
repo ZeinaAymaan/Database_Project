187a33178f2ddc58be1f9a4a745ebb1b91e0e17a
sara_a.wasfy@hotmail.com <sara_a.wasfy@hotmail.com>
2022-12-28 22:07:17 +0200

    sara akher haga awyyy

--- WebApplication1/Views/ViewStadiumInfoSM.aspx.cs
+++ WebApplication1/Views/ViewStadiumInfoSM.aspx.cs
@@ -11,8 +11,8 @@ namespace WebApplication1.Views
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            SqlDataSource1.SelectCommand = "SELECT Stadium.stadiumName, Stadium.stadiumLocation, Stadium.Capacity FROM Stadium INNER JOIN stadiumManager ON Stadium.ID = stadiumManager.Stadium_ID AND stadiumManager.stadiumManagerName = ";
-
+            SqlDataSource1.SelectCommand = "SELECT Stadium.stadiumName, Stadium.stadiumLocation, Stadium.Capacity FROM Stadium INNER JOIN stadiumManager ON Stadium.ID = stadiumManager.Stadium_ID AND stadiumManager.stadiumManagerName = '" + WebForm1.usernamee + "'";
+            SqlDataSource2.SelectCommand = "SELECT allPendingRequests_1.RepresentativeName, Club.clubName, allPendingRequests_1.GuestClubName, allPendingRequests_1.startTime, Match.endTime, hostRequest.Status\r\nFROM     Match INNER JOIN\r\n                  hostRequest ON Match.ID = hostRequest.Match_ID INNER JOIN\r\n                  clubRepresentative INNER JOIN\r\n                  Club ON clubRepresentative.club_ID = Club.ID ON Match.Host_ID = Club.ID INNER JOIN\r\n                  dbo.allPendingRequests('"+ WebForm1.usernamee +"') AS allPendingRequests_1 ON clubRepresentative.Name = allPendingRequests_1.RepresentativeName";
         }
     }
 }
--- WebApplication1/Views/ViewStadiumInfoSM.aspx.designer.cs
+++ WebApplication1/Views/ViewStadiumInfoSM.aspx.designer.cs
@@ -24,6 +24,15 @@ namespace WebApplication1.Views
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// Menu1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Menu Menu1;
+
+        /// <summary>
         /// GridView1 control.
         /// </summary>
         /// <remarks>
@@ -40,5 +49,50 @@ namespace WebApplication1.Views
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
+
+        /// <summary>
+        /// GridView2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView2;
+
+        /// <summary>
+        /// SqlDataSource2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource2;
+
+        /// <summary>
+        /// DropDownList1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
+
+        /// <summary>
+        /// AcceptButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button AcceptButton;
+
+        /// <summary>
+        /// RejectButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button RejectButton;
     }
 }
