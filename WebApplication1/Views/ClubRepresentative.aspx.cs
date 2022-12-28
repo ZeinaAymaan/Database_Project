62d7fc118183a5cd200a5855cfca32c655b3725d
Mayar Bahnacy <mayar.bahnacy14@gmail.com>
2022-12-28 23:00:33 +0200

    cr

--- WebApplication1/Views/ClubRepresentative.aspx.cs
+++ WebApplication1/Views/ClubRepresentative.aspx.cs
@@ -17,13 +17,34 @@ namespace WebApplication1.Views
         protected void Page_Load(object sender, EventArgs e) { 
             db.openConnection();
             string username = WebForm1.usernamee;
-            string viewClubInfoQuery = "select c.ID, c.clubName, c.clubLocation\r\nfrom Club as c inner join clubRepresentative as cr on c.ID=cr.club_ID\r\nwhere cr.clubRepresentativeUsername = '" + username + "'";
+            string viewClubInfoQuery = "select c.ID, c.clubName, c.clubLocation\r\nfrom Club as c inner join clubRepresentative as cr on c.ID=cr.club_ID\r\nwhere cr.clubRepresentativeUsername  = '"+ username +"';";
             SqlCommand clubinfo = new SqlCommand(viewClubInfoQuery, db.con);
             SqlDataReader clubinforeader = clubinfo.ExecuteReader();
-            Label1.Text = (string)clubinforeader["ID"];
-            Label2.Text = (string)clubinforeader["clubName"];
-            Label3.Text = (string)clubinforeader["clubLocation"];
+            while(clubinforeader.Read())
+            {
+                Label1.Text = clubinforeader["ID"].ToString();
+                Label2.Text = (string)clubinforeader["clubName"];
+                Label3.Text = (string)clubinforeader["clubLocation"];
+            }
+            clubinforeader.Close();
+
+            MultiView1.ActiveViewIndex = 0;
+            string viewUpcominClubMatchesQuery = "select c1name as 'Host club', c2name as 'Guest club', m.startTime as 'Start Time', m.endTime 'End Time'\r\n" +
+           "from upcomingMatchesOfClub ('" + Label2.Text + "') inner join Club c on c1name= c.clubName inner join Match m on c.ID=m.Host_ID";
+            SqlCommand viewUpcomingClubinfo = new SqlCommand(viewUpcominClubMatchesQuery, db.con);
+            SqlDataReader clubUpcominginforeader = viewUpcomingClubinfo.ExecuteReader();
+            if (clubUpcominginforeader.Read())
+            {
+                GridView1.DataSource = clubUpcominginforeader;
+                GridView1.DataBind();
+            }
+            else
+            {
+                Label4.Text = "Your Club Has No Upcoming Matches";
+            }
         }
+
+      
 
         // The return type can be changed to IEnumerable, however to support
         // paging and sorting, the following parameters must be added:
